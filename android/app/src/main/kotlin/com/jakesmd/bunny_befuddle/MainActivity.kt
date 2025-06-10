f8c1f03978c6ed39dcc2b79840060321c66bf465
JakesMD <71591438+JakesMD@users.noreply.github.com>
2025-06-10 13:56:16 +0200

    🎉 Initial commit

---
+++ android/app/src/main/kotlin/com/jakesmd/bunny_befuddle/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jakesmd.bunny_befuddle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'dart:core';
+
+import 'package:flame/game.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(GameWidget(game: FlameGame()));
